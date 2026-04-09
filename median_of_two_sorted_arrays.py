4c89a1179258f60cc2f0095b8916284bda401a4d
Torikul Islam Oli <olisheikh707@gmail.com>
2026-04-09 17:28:35 +0200

    Modified median of two  sorted array

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -4,14 +4,10 @@
 num1 = [1,2,3,4,5]
 num2 = [6,7,8,9,10,11,12,13,14,15,16,17]
 
-num3 = (num1 + num2).sort()
+num3 = (num1 + num2)
+num3.sort()
 
-print()
-
-if length_num3 % 2:
-    print(num3[(length_num3 // 2)-1])
-    print(num3[(length_num3 // 2)])
-    print((num3[(length_num3 // 2)-1] + num3[(length_num3 // 2)])/2)
+if len(num3) % 2:
+    print(num3[len(num3)//2])
 else:
-    print(round(length_num3/2))
-    print(num3[round(length_num3/2) - 1])
+    print((num3[len(num3)//2 - 1 ] + num3[len(num3)//2])/2)
